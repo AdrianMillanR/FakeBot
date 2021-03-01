d1b2cbf5761eb4105a006b920b016842706f6325
Adrian Millan <adrian231998@gmail.com>
2021-03-01 10:35:37 -0600

    agregamos un evento al botón de enviar para que se agreguen los mensajes, tanto del usuario como del Bot

--- app/src/main/java/com/adrian/fakebot/MainActivity.java
+++ app/src/main/java/com/adrian/fakebot/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.adrian.fakebot.databinding.ActivityMainBinding;
 
@@ -11,21 +12,50 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private  ArrayList<Message> messageList= new ArrayList<>();
+    private ArrayList<String> botAnswers= new ArrayList<>();
+    private Integer idMessage=1;
+    private MessageAdapter adapter= new MessageAdapter();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding= ActivityMainBinding.inflate(getLayoutInflater());
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         binding.messagesRecycler.setLayoutManager(new LinearLayoutManager(this));
 
-        ArrayList<Message> messageList= new ArrayList<>();
-        messageList.add(new Message(1,"¿Comeré Tacos hoy?", false));
-        messageList.add(new Message(2,"No", true));
-        messageList.add(new Message(3,"¿Viviré eternamente?", false));
-        messageList.add(new Message(4,"Si", true));
 
-        MessageAdapter adapter= new MessageAdapter();
+        botAnswers.add("Si");
+        botAnswers.add("No");
+        botAnswers.add("Pregunta de nuevo");
+        botAnswers.add("Es muy probable");
+        botAnswers.add("No lo creo");
+        botAnswers.add("No lo sé");
+        botAnswers.add("Tal vez");
+
+
         binding.messagesRecycler.setAdapter(adapter);
-        adapter.submitList(messageList);
+        //adapter.submitList(messageList);
+
+        binding.sendMessageButton.setOnClickListener(v -> {
+            addMessages();
+        });
+
+    }
+
+    private void addMessages(){
+        String humanMessage= binding.messageText.getText().toString();
+        if (humanMessage.isEmpty()){
+            Toast.makeText(this,"You must write a message",Toast.LENGTH_SHORT).show();
+        }else{
+            messageList.add(new Message(idMessage,humanMessage,false));
+            idMessage++;
+            int randomPosition= (int)Math.random()*8;
+            messageList.add(new Message(idMessage,botAnswers.get(randomPosition),true));
+            idMessage++;
+            adapter.submitList(messageList);
+            binding.messageText.setText("");
+        }
     }
 }
